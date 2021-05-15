14e1826e92efd6648bca48e5683c987d2cf10de3
hakhan88 <hakhan88@gmail.com>
2021-05-15 18:29:45 +0800

    cosmetic updates

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,18 @@
-$(document).ready(function() {
-	$(".scroll").click(function(event){		
-	event.preventDefault();
-	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
-	});
+$(document).ready(function () {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    });
 });
 
-$(document).ready(function() {	
-	var windowHeight = $(window).height();
-			
-	$(".work").css('height', windowHeight),
-	$("#contact").css('height', windowHeight);
-	
+$(document).ready(function () {
+    // var windowHeight = $(window).height();
+    // $(".work").css('height', windowHeight);
+
 });
 
-$(window).resize(function() {	
-	var windowHeight = $(window).height();
-			
-	$(".work").css('height', windowHeight),
-	$("#contact").css('height', windowHeight);
-	
+$(window).resize(function () {
+    // var windowHeight = $(window).height();
+    // $(".work").css('height', windowHeight);
+
 });
